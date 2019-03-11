a5c9d7038c0898e079f40dc60199db7097d36cfc
Andrew Miscoe <armiscoe@gmail.com>
2019-03-11 02:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-function takeANumber()
+var katzDeli = []; //empty katzDeli
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  //
+}
