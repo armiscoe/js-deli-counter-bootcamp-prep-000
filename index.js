b7677a83accaad1673606fabb13b000a98e4f2da
Andrew Miscoe <armiscoe@gmail.com>
2019-03-11 02:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var katzDeli = []; //empty katzDeli
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-  //
+  
 }
+  //defines takeANumber function which adds a new name to end of katzDeli Array(push). returns a string which states their current position in line
+  
+  
